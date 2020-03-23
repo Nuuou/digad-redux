32af1768de205ea3310375d6cbf2396dba2634c7
Zeth Schlenker <zschlenker2011@gmail.com>
2020-03-23 11:24:56 -0500

    Modifying sprite task to accept multiple configured through package

--- scripts/sprite.js
+++ scripts/sprite.js
@@ -5,78 +5,91 @@ const imageminPngquant = require('imagemin-pngquant');
 const Spritesmith = require('spritesmith');
 const glob = require('glob');
 const templater = require('spritesheet-templates');
-const sass = require('node-sass');
+const packageJson = require('../package.json');
 
-const pixelRatio = 2;
-const spritePattern = '_ui/skin/src/img/sprite/**/*.png';
-const spriteSheetImageFilename = 'sprite.png';
-const spriteSheetImageOutput = '_ui/skin/src/img';
-const spriteSheetStyleFilename = '_sprite.scss';
-const spriteSheetStyleOutput = '_ui/skin/src/sass';
+const spriteData = packageJson.digad.sprites;
+const spriteConfigDefault = {
+  pixelRatio: 2,
+  sourcePattern: '_ui/skin/src/img/sprite/**/*.png',
+  outputImagePath: '_ui/skin/src/img',
+  outputImageFilename: 'sprite.png',
+  outputStylePath: '_ui/skin/src/sass',
+  outputStyleFilename: '_sprite.scss',
+  template: '_ui/skin/src/templates/customtemplate.template.handlebars',
+  padding: 2,
+  algorithm: 'binary-tree',
+  algorithmOptions: {},
+};
 
-const sprites = glob.sync(spritePattern);
-Spritesmith.run({
-  src: sprites,
-  padding: 10,
-}, (err, result) => {
-  if (err) {
-    throw err;
-  }
+Object.values(spriteData).forEach((spriteConfigCustom) => {
+  const spriteConfig = {
+    ...spriteConfigDefault,
+    ...spriteConfigCustom,
+  };
 
-  /**
-   * GENERATE SPRITE FILE.
-   */
-  //fs.writeFileSync(path.resolve(spriteSheetImageOutput), result.image);
+  const sprites = glob.sync(spriteConfig.sourcePattern);
+  Spritesmith.run({
+    src: sprites,
+    padding: spriteConfig.padding,
+    algorithm: spriteConfig.algorithm,
+    algorithmOpts: spriteConfig.algorithmOptions,
+  }, (err, result) => {
+    if (err) {
+      throw err;
+    }
 
-  const spritesCoordinates = [];
-  Object.entries(result.coordinates).forEach(([key, val]) => {
-    const name = path.basename(key, path.extname(key));
-    spritesCoordinates.push({
-      x: Math.ceil(val.x / pixelRatio),
-      y: Math.ceil(val.y / pixelRatio),
-      width: Math.ceil(val.width / pixelRatio),
-      height: Math.ceil(val.height / pixelRatio),
-      name
-    })
-  });
+    /**
+     * GENERATE SPRITE FILE.
+     */
 
-  /**
-   * IMAGE MINIFICATION.
-   */
-  (async () => {
-    imagemin.buffer(result.image, {
-      plugins: [
-        imageminPngquant({
-          speed: 3,
-          quality: [0.4, 1.0],
-          strip: true,
-        }),
-      ],
-    }).then((output) => {
-      fs.writeFile(path.resolve(spriteSheetImageOutput, spriteSheetImageFilename), output, (imageminErr) => {
-        if (imageminErr) {
-          throw imageminErr;
-        }
+    const spritesCoordinates = [];
+    Object.entries(result.coordinates).forEach(([key, val]) => {
+      const name = path.basename(key, path.extname(key));
+      spritesCoordinates.push({
+        x: Math.ceil(val.x / spriteConfig.pixelRatio),
+        y: Math.ceil(val.y / spriteConfig.pixelRatio),
+        width: Math.ceil(val.width / spriteConfig.pixelRatio),
+        height: Math.ceil(val.height / spriteConfig.pixelRatio),
+        name,
       });
     });
-  })();
 
-  /**
-   * GENERATE SASS FILE.
-   */
-  const customTemplate = fs.readFileSync(path.resolve('_ui/skin/src/templates/customtemplate.template.handlebars'), 'utf8');
-  templater.addHandlebarsTemplate('retinaOnly', customTemplate);
+    /**
+     * IMAGE MINIFICATION.
+     */
+    (async () => {
+      imagemin.buffer(result.image, {
+        plugins: [
+          imageminPngquant({
+            speed: 3,
+            quality: [0.4, 1.0],
+            strip: true,
+          }),
+        ],
+      }).then((output) => {
+        fs.writeFile(path.resolve(spriteConfig.outputImagePath, spriteConfig.outputImageFilename), output, (imageminErr) => {
+          if (imageminErr) {
+            throw imageminErr;
+          }
+        });
+      });
+    })();
 
-  const template = templater({
-    sprites: spritesCoordinates,
-    spritesheet: {
-      width: Math.ceil(result.properties.width / pixelRatio),
-      height: Math.ceil(result.properties.height / pixelRatio),
-      //image: path.relative('./', spriteSheetImageOutput),
-      //image: '../img/sprite.png',
-      image: path.join(path.relative(spriteSheetStyleOutput, spriteSheetImageOutput), spriteSheetImageFilename),
-    }
-  }, { format: 'retinaOnly'})
+    /**
+     * GENERATE SASS FILE.
+     */
+    const customTemplate = fs.readFileSync(path.resolve(spriteConfig.template), 'utf8');
+    templater.addHandlebarsTemplate('retinaOnly', customTemplate);
 
-  fs.writeFileSync(path.resolve(spriteSheetStyleOutput, spriteSheetStyleFilename), template);
+    const template = templater({
+      sprites: spritesCoordinates,
+      spritesheet: {
+        width: Math.ceil(result.properties.width / spriteConfig.pixelRatio),
+        height: Math.ceil(result.properties.height / spriteConfig.pixelRatio),
+        image: path.join(path.relative(spriteConfig.outputStylePath, spriteConfig.outputImagePath), spriteConfig.outputImageFilename),
+      },
+    }, { format: 'retinaOnly' });
+
+    fs.writeFileSync(path.resolve(spriteConfig.outputStylePath, spriteConfig.outputStyleFilename), template);
+  });
 });
